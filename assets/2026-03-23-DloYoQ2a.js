155b747922fb7f362012ee0f958ffd2e5933db45
shin-noda <shin-noda@users.noreply.github.com>
2026-03-24 11:34:20 +0000

    deploy: 2309615fab41c5bed46ebd7af661d4302114b999

---
+++ assets/2026-03-23-DloYoQ2a.js
@@ -0,0 +1,13 @@
+var e=`---
+date: 2026-03-22
+title: Worked on a LeetCode Problem
+objectives:
+  - LeetCode
+categories:
+  - Python
+  - Greedy Algorithm
+  - Array / String
+actions:
+  - Worked on Problem 14 (Longest Common Prefix)
+summary: I learned how to apply a greedy algorithm to find the longest common prefix.
+---`;export{e as default};

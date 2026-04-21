df88976c263329cba3781af4b953981b6e1e4d6f
shin-noda <shin-noda@users.noreply.github.com>
2026-04-21 20:11:34 +0000

    deploy: a6ecf69cc157b73b59d0861864dced58aefb51ca

--- assets/2026-03-17-3joQP6ii.js
+++ assets/2026-03-17-3joQP6ii.js
@@ -1,11 +1,11 @@
 var e=`---
 date: 2026-03-17
-title: Completed the basics of the "Learning Journey" project
+title: Completed the basics of the Learning Journey project
 objectives:
   - Project: Learning Journey
 categories:
   - React
 actions:
-  - Completed the core functionality of the "learning-journey" project
+  - Completed the core functionality of the learning-journey project
 summary: I worked on the Learning Journey, and I'm ready to keep a record of my learning from now on.
 ---`;export{e as default};

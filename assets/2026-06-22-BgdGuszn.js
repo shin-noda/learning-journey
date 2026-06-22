223e1cf5bd584c84e31ee74928edf882350205d5
shin-noda <shin-noda@users.noreply.github.com>
2026-06-22 21:02:32 +0000

    deploy: aa4028a4ff3aa4e3d3740de47cb4396e9dc4cc36

---
+++ assets/2026-06-22-BgdGuszn.js
@@ -0,0 +1,17 @@
+var e=`---
+date: 2026-06-22
+title: Worked on LeetCode Problems
+objectives:
+  - LeetCode
+categories:
+  - LeetCode
+  - Problem Set 97 (My Study Plan)
+  - Probability and Statistics
+  - Sweep Line
+actions:
+ - Solved 1093 (Statistics from a Large Sample)
+ - Solved 1227 (Airplane Seat Assignment Probability)
+ - Solved 1467 (Probability of a Two Boxes Having The Same Number of Distinct Balls)
+ - Solved 986 (Interval List Intersections)
+summary: Started working on the Probability and Statistics section on Problem Set 97.
+---`;export{e as default};

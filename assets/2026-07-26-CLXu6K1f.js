01e01767b16021f785f5ce540c37b1f049a76a4b
shin-noda <shin-noda@users.noreply.github.com>
2026-07-26 18:06:31 +0000

    deploy: 115a65659ded95cb4a40e9a571e52ce02ac203d4

---
+++ assets/2026-07-26-CLXu6K1f.js
@@ -0,0 +1,11 @@
+var e=`---
+date: 2026-07-26
+title: Worked on Deep Learning with Python
+objectives:
+  - Deep Learning with Python
+categories:
+  - Deep Learning with Python
+actions:
+- Read Chapter 1
+summary: Started reading Deep Learning with Python.
+---`;export{e as default};

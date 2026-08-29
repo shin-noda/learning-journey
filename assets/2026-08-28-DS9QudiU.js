fb2f1d9b03e8f1a9d3ab8931272b28e35aae55d2
shin-noda <shin-noda@users.noreply.github.com>
2026-08-29 03:58:42 +0000

    deploy: 8cb77d129ea9e035e1f001db07e8b69dd0d5ae69

---
+++ assets/2026-08-28-DS9QudiU.js
@@ -0,0 +1,16 @@
+var e=`---
+date: 2026-08-28
+title: Submitted my Titanic solution on Kaggle and worked on Deep Learning with Python
+objectives:
+  - Deep Learning with Python
+  - Kaggle
+categories:
+  - Deep Learning with Python
+  - Kaggle
+  - Titanic - Machine Learning from Disaster
+actions:
+- Worked on Chapter 5
+- Worked on Chapter 6
+- Submitted my solution for Titanic - Machine Learning from Disaster
+summary: Worked on the Titanic solution on Kaggle and Deep Learning with Python.
+---`;export{e as default};

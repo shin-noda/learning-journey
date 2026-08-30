475c71c92d5c383255a5de12969da34842888877
shin-noda <shin-noda@users.noreply.github.com>
2026-08-30 10:51:37 +0000

    deploy: 9e602382ba865127a98f0de234181aa4b119c868

---
+++ assets/2026-08-30-DcthmhH-.js
@@ -0,0 +1,12 @@
+var e=`---
+date: 2026-08-28
+title: Submitted my Digit Recognizer on Kaggle
+objectives:
+  - Kaggle
+categories:
+  - Kaggle
+  - Digit Recognizer
+actions:
+- Submitted my solution for Digit Recognizer
+summary: Worked on the Digit Recognizer solution on Kaggle.
+---`;export{e as default};

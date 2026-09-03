c821abad0734697cf529193d663adeafe0367432
shin-noda <shin-noda@users.noreply.github.com>
2026-09-03 09:23:00 +0000

    deploy: f4d71800b494a30c40cdfd755545c32853f0f33d

---
+++ assets/2026-09-03-se5gh75X.js
@@ -0,0 +1,11 @@
+var e=`---
+date: 2026-09-03
+title: Worked on Deep Learning with Python
+objectives:
+  - Deep Learning with Python
+categories:
+  - Deep Learning with Python
+actions:
+- Worked on Chapter 12
+summary: Worked on Deep Learning with Python.
+---`;export{e as default};
